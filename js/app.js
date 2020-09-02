5c62f065190b390ea697c02dd0c02ceb6a4f57b1
Robert Pearce <me@robertwpearce.com>
2020-09-01 21:01:41 -0400

    display crop future tables

--- js/app.js
+++ js/app.js
@@ -21,47 +21,67 @@
     console.log('get error')
   }
 
-  function handleCornLoad(data) {
-    // todo store in localStorage
-    console.log({ corn: data })
-  }
+  function handleLoad(type) {
+    return function (data) {
+      // todo store in localStorage
+      //console.log({ [type]: data })
 
-  function handleWheatLoad(data) {
-    // todo store in localStorage
-    console.log({ wheat: data })
-  }
+      if (!data || !data.dataset) {
+        return
+      }
 
-  function handleSoybeanLoad(data) {
-    // todo store in localStorage
-    console.log({ soybean: data })
-  }
+      var el = d.querySelector('[data-crop="' + type + '"]');
+      var ds = data.dataset;
+      var ths = (ds.column_names || []).slice(0, 9);
+      var tds = (ds.data[0] || []).slice(0, 9);
 
-  function handleCottonLoad(data) {
-    // todo store in localStorage
-    console.log({ cotton: data })
+      var table = '<table class="bcc bse0e0dc bw1001 fs18 lh17 ffss mw100p w100p">' +
+        '<thead>' +
+          '<caption class="mb1 tal">' + ds.name + '</caption>' +
+          '<tr>' +
+            ths.map(function (x) {
+              return '<th class="bge4eaee bse0e0dc bw0110 fwn mw18 p6x8 tal vat">' +
+                x +
+              '</th>'
+            }).join('') +
+          '</tr>' +
+        '</thead>' +
+        '<tbody>' +
+          '<tr>' +
+            tds.map(function (x) {
+              return '<td class="bse0e0dc bw0110 p6x8">' +
+                (x != null ? x : 'n/a') +
+              '</td>'
+            }).join('') +
+          '</tr>' +
+        '</tbody>' +
+      '</table>'
+
+      el.innerHTML = table
+    }
   }
 
   get(
     handleError,
-    handleCornLoad,
+    handleLoad('corn'),
     { url: 'https://www.quandl.com/api/v3/datasets/CHRIS/CME_C1.json?api_key=Yr8iCRc9Rz_x9k4LyNw3&limit=1' }
   )
 
   get(
     handleError,
-    handleWheatLoad,
+    handleLoad('wheat'),
     { url: 'https://www.quandl.com/api/v3/datasets/CHRIS/CME_W1.json?api_key=Yr8iCRc9Rz_x9k4LyNw3&limit=1' }
   )
 
   get(
     handleError,
-    handleSoybeanLoad,
+    handleLoad('soybeans'),
     { url: 'https://www.quandl.com/api/v3/datasets/CHRIS/CME_S1.json?api_key=Yr8iCRc9Rz_x9k4LyNw3&limit=1' }
   )
 
   get(
     handleError,
-    handleCottonLoad,
+    handleLoad('cotton'),
     { url: 'https://www.quandl.com/api/v3/datasets/CHRIS/ICE_CT1.json?api_key=Yr8iCRc9Rz_x9k4LyNw3&limit=1' }
   )
 })(window, document);
